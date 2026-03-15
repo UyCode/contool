3af99932dfe4ab28fd504ded35d5354fa106f0f4
UyCode <Hyper-Hack@outlook.com>
2026-03-15 12:52:00 +0800

    use mathjs for evaluation instead of direct eval

--- src/calc.js
+++ src/calc.js
@@ -1,7 +1,9 @@
+import { evaluate } from 'mathjs';
+
 export function evaluateExpression(expr) {
   const cleaned = expr
     .replace(/0x([0-9a-f]+)/gi,(m,v)=>parseInt(v,16))
     .replace(/0b([01]+)/gi,(m,v)=>parseInt(v,2))
     .replace(/0o([0-7]+)/gi,(m,v)=>parseInt(v,8));
-  console.log(eval(cleaned));
+  console.log(evaluate(cleaned));
 }
